cde5ccde4ea451bc84b24e1f60c476c5f48f669a
terbeche <termus96@gmail.com>
2022-09-13 22:42:17 +0100

    Refactor migration code

--- db/migrate/20220913101411_add_author_id_to_comment.rb
+++
@@ -1,6 +0,0 @@
-class AddAuthorIdToComment < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :author_id, :integer
-    add_index :comments, :author_id
-  end
-end
--- db/migrate/20220913101419_add_author_id_to_like.rb
+++
@@ -1,6 +0,0 @@
-class AddAuthorIdToLike < ActiveRecord::Migration[7.0]
-  def change
-    add_column :likes, :author_id, :integer
-    add_index :likes, :author_id
-  end
-end
