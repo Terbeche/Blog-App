395d876c79ee9a7d1bf1b9d058634aaf4fc2ed2d
terbeche <termus96@gmail.com>
2022-09-13 10:51:22 +0100

    Add migrations for adding columns and foreign keys

---
+++ db/migrate/20220913093844_add_column_to_user.rb
@@ -0,0 +1,10 @@
+class AddColumnToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :photo, :string
+    add_column :users, :bio, :text
+    add_column :users, :updatedAt, :timestamp
+    add_column :users, :createdAt, :timestamp
+    add_column :users, :postsCounter, :integer
+  end
+end
