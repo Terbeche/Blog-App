a02da95dacf7e1ca17fd38955c7211a0ef0f74c4
terbeche <termus96@gmail.com>
2022-09-21 13:39:33 +0100

    Add views and edit actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
 
-  def show; end
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  #  validates :name, presence: true, allow_blank: false
-  #  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true, allow_blank: false
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_three_posts
     posts.order(updated_at: :desc).last(3)
