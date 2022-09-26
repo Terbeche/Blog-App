90a314319c78ad1e876c821889e1f02516d31f73
terbeche <termus96@gmail.com>
2022-09-26 15:59:59 +0100

    Add intergration tests for post index and show views

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 RSpec.describe 'Users index', type: :feature do
   before(:each) do
+    # rubocop: disable Layout/LineLength
     @first_user = User.create(name: 'Tom',
                               photo: 'https://www.thoughtco.com/thmb/0I-Uw-0rcc6MUzcZJauNGKR9JzA=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/male-computer-programmer-using-laptop-at-desk-in-office-755650739-5c5bb32346e0fb0001f24d3d.jpg', bio: 'Teacher from Mexico.', posts_counter: 0)
     @second_user = User.create(name: 'Lilly',
                                photo: 'https://image.benq.com/is/image/benqco/monitor-for-programmer-1?$ResponsivePreset$&wid=1468', bio: 'Teacher from Poland.', posts_counter: 0)
     @third_user = User.create(name: 'Mustapha',
                               photo: 'https://www.mooc.org/hs-fs/hubfs/learn-programming-career-jpg.jpeg?width=500&name=learn-programming-career-jpg.jpeg', bio: 'Teacher from Algeria.', posts_counter: 0)
+    # rubocop: enable Layout/LineLength
     visit users_path
   end
-  describe 'index page' do
+  describe 'user index page' do
     it 'shows the usernames of all users' do
       expect(page).to have_content('Tom')
       expect(page).to have_content('Lilly')
